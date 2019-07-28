9761a6c0bccf65d8acb8b73b4778a9f77117d255
vyashole <vyashole@gmail.com>
2019-07-28 16:24:03 +0530

    git signing

--- setup.sh
+++ setup.sh
@@ -16,20 +16,21 @@ sudo apt -y install curl git mercurial make binutils bison gcc build-essential c
 
 # install pip packages and then pyenv
 sudo pip3 install thefuck virtualenvwrapper awscli saws
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+rm -rf ~/.pyenv && git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-#  install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+rm -rf ~/.nvm && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
 
-# install codium repo 
+# install codium repo
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
 echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee /etc/apt/sources.list.d/69-codium.list
 sudo apt update
 # install codium
 sudo apt -y install codium
 # install extensions
-cat .vscode_extensions | xargs -I {} codium --install-extension {}
+cat vscode/.vscode_extensions | xargs -I {} codium --install-extension {}
+
+#  install oh my zsh
+rm -rf ~/.oh-my-zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
